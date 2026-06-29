1a33b0d01c4a4a8db98b9874a0d3522aa7c0d818
Fabiana Leonardo <es.fabileo@gmail.com>
2026-06-29 13:12:46 +0200

    inicial commit

---
+++ src/test/java/com/femcoders/mundotech/MundotechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.femcoders.mundotech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MundotechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
