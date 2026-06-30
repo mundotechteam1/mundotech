2e190bbf0ac0d44bbceb00db2bd3710f58baa64b
SiuzannaVach <siuzannavachaganian@gmail.com>
2026-06-30 14:52:38 +0200

    feat: add User entity and database mappings

--- src/main/java/com/femcoders/mundotech/entity/User.java
+++ src/main/java/com/femcoders/mundotech/entity/User.java
@@ -1,27 +1,47 @@
 package com.femcoders.mundotech.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
-
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
 import java.util.List;
-
 
 @Entity
 @Table(name = "users")
 @Data
 public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @NotBlank
     private String name;
+
+    @NotBlank
+    @Email
+    @Column(unique = true)
     private String email;
+
+    @NotBlank
+    @Size(min = 6)
     private String password;
 
-    @ManyToOne
-    @JoinColumn(name = "role_id")
-    private Role role;
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    @ManyToMany
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private List<Role> roles;
 
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
     @OneToMany(mappedBy = "author", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Article> articles;
 }
-
