b60e3b71d295f5f56f0c7ddcf9f143851aafca3c
SiuzannaVach <siuzannavachaganian@gmail.com>
2026-06-30 13:12:16 +0200

    feat: changes of users

--- src/main/java/com/femcoders/mundotech/entity/User.java
+++ src/main/java/com/femcoders/mundotech/entity/User.java
@@ -3,6 +3,8 @@ package com.femcoders.mundotech.entity;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.util.List;
+
 
 @Entity
 @Table(name = "users")
@@ -14,6 +16,12 @@ public class User {
     private String name;
     private String email;
     private String password;
+
+    @ManyToOne
+    @JoinColumn(name = "role_id")
     private Role role;
 
+    @OneToMany(mappedBy = "author", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Article> articles;
 }
+
