da60adb3353ca6f3aa58d1719d712f8dcbd4a465
SiuzannaVach <siuzannavachaganian@gmail.com>
2026-06-30 09:47:49 +0200

    feat:crate user entity

--- src/main/java/com/femcoders/mundotech/entity/User.java
+++ src/main/java/com/femcoders/mundotech/entity/User.java
@@ -1,5 +1,19 @@
 package com.femcoders.mundotech.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+
+@Entity
+@Table(name = "users")
+@Data
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String email;
+    private String password;
+    private Role role;
 
 }
