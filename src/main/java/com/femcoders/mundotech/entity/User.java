574cda7213fab921d9c9a2e40265c177ba64f213
SiuzannaVach <siuzannavachaganian@gmail.com>
2026-06-30 15:17:11 +0200

    feat: add user fields validation with spring-boot-starter-validation

--- src/main/java/com/femcoders/mundotech/entity/User.java
+++ src/main/java/com/femcoders/mundotech/entity/User.java
@@ -1,19 +1,47 @@
 package com.femcoders.mundotech.entity;
 
 import jakarta.persistence.*;
-import lombok.Data;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+import java.util.List;
 
 @Entity
 @Table(name = "users")
 @Data
 public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-    private String name;
-    private String email;
-    private String password;
-    private Role role;
 
+    private String name;
+
+    @NotBlank
+    @Email
+    @Column(unique = true)
+    private String email;
+
+    @NotBlank
+    @Size(min = 6)
+    private String password;
+
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    @ManyToMany
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private List<Role> roles;
+
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    @OneToMany(mappedBy = "author", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Article> articles;
 }
