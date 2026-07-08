af1bac3f9e12c9536034563a9b1a0bfc2f4162d3
AidaG91 <aida.garmus@gmail.com>
2026-07-08 21:52:18 +0200

    feat: add basic html structure to Login.jsx. Update fonts as per Figma

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,3 +1,17 @@
+import LoginForm from "../../components/login-form/LoginForm";
+import styles from "./Login.module.scss";
+
 export default function Login() {
-  return <></>;
+  return (
+    <>
+      <main className={styles.loginPage}>
+        <section className={styles.loginContainer}>
+          <h1 className={styles.loginHeading}>Internal Access</h1>
+          <p className={styles.loginDescription}>Please provide your credentials to enter the editorial suite.</p>
+
+          <LoginForm />
+        </section>
+      </main>
+    </>
+  );
 }
